c96fa3d7de4dd672da1d81a73cb212962fe84bd5
José Moreno <chpmoreno@gmail.com>
2024-10-18 11:30:44 +0200

    lecture changes

--- slides/02/functions.py
+++ slides/02/functions.py
@@ -5,32 +5,30 @@ def greet(name: str) -> None:
     """Greets a person by name.
     Args:
         name (str): The name of the person to greet.
-    Returns:
-        None
     """
     print(f"Hello, {name}!")
 
-greet("Raul")  # Output: Hello, Raul!
+greet(name="Raul")  # Output: Hello, Raul!
 
-def add_numbers(x: int, y: int) -> int:
+def add_numbers(number_1: int, number_2: int) -> int:
     """
     Adds two numbers and returns the result.
     
     Args:
-        x (int): The first number
-        y (int): The second number
+        number_1 (int): The first number
+        number_2 (int): The second number
     
     Returns:
-        int: The sum of x and y
+        int: The sum of number_1 and number_2
     """
-    return x + y
+    return number_1 + number_2
 
 # Positional arguments
 RESULT_1 = add_numbers(10, 5)
 print(RESULT_1) # Output: 15
 
 # Keyword arguments
-RESULT_2 = add_numbers(x=10, y=5)
+RESULT_2 = add_numbers(number_1=10, number_2=5)
 print(RESULT_2) # Output: 15
 
 def calculate_area(length: int, width: int) -> int:
@@ -49,12 +47,17 @@ def calculate_area(length: int, width: int) -> int:
 RECTANGLE_AREA = calculate_area(4, 6)
 print(RECTANGLE_AREA)  # Output: 24
 
-def greet_with_message(name: str = "Guest", message: str = "", age: Optional[int] = None) -> None:
+def greet_with_message(
+    name: str = "Guest",
+    message: str = "",
+    age: Optional[int] = None
+) -> None:
     """Prints a greeting message with a name and an optional message.
 
     Args:
         name (str): The name of the person to greet. Defaults to "Guest".
         message (str): The message to use. Defaults to "Welcome!".
+        age (int, optional): The age of the person. Defaults to None.
     """
     if not message:
         message = "Welcome!"
@@ -65,4 +68,9 @@ def greet_with_message(name: str = "Guest", message: str = "", age: Optional[int
 
 greet_with_message()  # Output: Welcome!, Guest!
 greet_with_message("Raul")  # Output: Welcome!, Raul!
-greet_with_message("Paula", "Hello")  # Output: Hello, Bob!
+greet_with_message("Paula", "Hello")  # Output: Hello, Paula!
+greet_with_message(
+    name="José",
+    message="Hello",
+    age=35
+)
